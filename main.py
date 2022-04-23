be578a6170f29c0777b3ffad9f859ed4ebc17ffa
Luke <shijiluke@gmail.com>
2022-04-23 16:18:48 +0800

    423

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import math
+
+from bokeh.palettes import Spectral10, Dark2
 from bokeh.plotting import figure, output_file, show
 import pandas as pd
 from bokeh.models import ColumnDataSource, HoverTool
 from bokeh.plotting import figure
+from bokeh.transform import factor_cmap
 from bs4 import BeautifulSoup
 from urllib.request import Request, urlopen
 
+pd.set_option('display.expand_frame_repr', False)
 url = 'https://hk.centanet.com/findproperty/list/transaction?q=e3CSEC7eUPrFVi8Vi6pg'
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
 webpage = urlopen(req).read()
@@ -53,21 +57,27 @@ for x in soup.find_all('div', {'class': 'right-block row-cxt'}):
     df.loc[len(df.index)] = [title, location, size, pricePerSqft, date, totalPrice]
 
 print(df)
+df['pricePerSqft'] = df['pricePerSqft'].astype(int)
 df = df.set_index('date')
 df.index = pd.to_datetime(df.index)
 df = df.sort_index()
+df['level'] = df['title'].apply(lambda x: x.split('_')[2])
 df['10ma'] = df['pricePerSqft'].rolling(10).mean()
 df['5ma'] = df['pricePerSqft'].rolling(5).mean()
-# df['date'] = df.index
+df['level'] = df['title'].apply(lambda x: x.split('_')[2])
+
 global_source = ColumnDataSource(pd.DataFrame())
 global_source.data = ColumnDataSource.from_df(df)
 
 graph = figure(title='prices chart', width=1000, x_axis_type="datetime")
 graph.xaxis.major_label_orientation = math.pi / 4
 graph.grid.grid_line_alpha = 0.3
-graph.add_tools(HoverTool(tooltips=[('date', '@date{%Y-%m-%d}'), ('pricePerSqft', '@pricePerSqft')],
-                          formatters={'@date': 'datetime'}, mode='vline'))
+graph.add_tools(HoverTool(tooltips=[('date', '@date{%Y-%m-%d}'), ('pricePerSqft', '@pricePerSqft'),
+                                    ('level','@level')],
+                          formatters={'@date': 'datetime'}, mode='mouse'))
 # graph.scatter(source=global_source)
-graph.circle(x='date', y='pricePerSqft', source=global_source)
+pal = Dark2[3]
+graph.circle(x='date', y='pricePerSqft', source=global_source, size=10,
+             fill_color=factor_cmap('level', palette=pal, factors=df['level'].unique()))
 print(df)
 show(graph)
